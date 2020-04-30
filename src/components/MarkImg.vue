6173595d8b8a6a3734010b6a2c3cfacba9f28700
liuchang1a2a <141303@supinfo.com>
2020-04-30 15:40:21 +0800

    webimg done

--- src/components/MarkImg.vue
+++ src/components/MarkImg.vue
@@ -1,9 +1,21 @@
 <template>
     <div v-cloak>
         <div  class="markbox">
-            <el-input v-model="text" placeholder="请输入内容" class="mark_txt"></el-input>
-            <el-upload class="avatar-uploader el-upload--text mark_uploader" 
-            :action="uploadUrl"
+            <el-menu :default-active="activeMenu" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-submenu index="1">
+                    <template slot="title">本地图片</template>
+                    <el-menu-item index="1-1">标识文字</el-menu-item>
+                    <el-menu-item index="1-2">标识图片</el-menu-item>
+                </el-submenu>
+                 <el-submenu index="2">
+                    <template slot="title">网络图片</template>
+                    <el-menu-item index="2-1">标识文字</el-menu-item>
+                    <el-menu-item index="2-2">标识图片</el-menu-item>
+                </el-submenu>
+            </el-menu>
+            <el-input v-if="activeMenu=='1-1'||activeMenu=='2-1'" v-model="text" placeholder="请输入内容" class="mark_txt"></el-input>
+            <el-upload v-if="activeMenu=='1-1'||activeMenu=='1-2'" class="avatar-uploader el-upload--text mark_uploader" 
+            :action="uploadUrl" 
             ref="upload"	   
             :show-file-list="false" 
             :on-success="handleSuccess" 
@@ -13,6 +25,7 @@
                 <div slot="tip" class="el-upload__tip" v-if="orgname==''">上传文件不超过10M</div>
                 <div slot="tip" class="el-upload__tip" v-else>{{orgname}}</div>
             </el-upload>
+            <el-input v-if="activeMenu=='2-1'||activeMenu=='2-2'" v-model="url" placeholder="请输入图片地址 http://" class="mark_txt"></el-input>
             <el-button size="small" type="primary" @click="preview">预览</el-button>
         </div>
         <!--
@@ -25,11 +38,14 @@ export default {
     name:'MarkImg',
     data(){
         return {
+            activeMenu:"1-1",
             text:'',
             uploadUrl:'',
             data:{},
             orgname:'',
-            path:''
+            path:'',
+            url:'',
+            downUrl:'',
         }
     },
     mounted:function(){
@@ -38,6 +54,47 @@ export default {
     methods:{
         initData:function(){
             this.uploadUrl = this.$axios.defaults.baseURL+"/img/upload";
+            this.downUrl = this.$axios.defaults.baseURL+"/img/download";
+        },
+        handleSelect:function(key, keyPath) {
+            this.activeMenu = key;
+            this.text='';
+            this.url='';
+            this.orgname='';
+            this.path='';
+        },
+       download:function(){
+            var that =this;
+            var path = '';
+            var postdata = this.$qs.stringify({
+                url: this.url,
+                path:'/file/test'
+            });
+            // 同步
+            // var axios = async function(){
+            //    return await this.$axios.post(this.downUrl, postdata).res;
+            // };
+            // var res = axios;
+            // var data =res.data;
+            // console.log(data);
+            // if(data.code=='0'){
+            //     var info = data.info;
+            //     path =info.path;
+            // }else{
+            //     this.showError(data.msg);
+            // }
+            // return path;
+
+            return  new Promise((resolve,reject)=>{
+                that.$axios.post(this.downUrl, postdata).then(res=>{
+                    var data = res.data;
+                    resolve(data.code==0?resolve(data.info.path):'');
+                }).catch(err=>{
+                    reject('');
+                });
+            });
+
+            
         },
         beforeUpload: function(file) {
             this.data.path = "/file/img";
@@ -48,7 +105,6 @@ export default {
             }
         },
         handleSuccess: function (res, file) {   
-             console.log(res);    
              if(res.code=='0'){
                  var info = res.info;
                  this.orgname = info.org;
@@ -57,22 +113,37 @@ export default {
                  this.showError(res.msg);
              }
         },
-        preview:function(){
-            if(this.path==''){
-                this.showError("请上传文件");
-                return false;
+        preview: async function(){
+            var path = '';
+            if(this.activeMenu=='1-1'){
+                if(this.path==''){
+                    this.showError("请上传文件");
+                    return false;
+                }
+                path = this.path;
+            }else if(this.activeMenu=='2-1'){
+                if(this.url==''){
+                    this.showError("请输入图片地址");
+                    return false;
+                }
+                path = await this.download();         
+                if(path==''){
+                    this.showError("图片解析失败");
+                    return false;
+                }
             }
             var ext = "png";
             var json = {
                 "text": this.text,
-                // "color":'',
+                    // "color":'',
                 "fsize":30,
-                // "fstyle":'',
-                "x":250,
-                "y":520
+                    // "fstyle":'',
+                // "x":250,
+                // "y":520
             };
-            var url = this.$axios.defaults.baseURL+"img/marktxt?path="+encodeURIComponent(this.path)+"&ext="+ext+"&json="+encodeURIComponent(JSON.stringify(json));
+            var url = this.$axios.defaults.baseURL+"img/marktxt?path="+encodeURIComponent(path)+"&ext="+ext+"&json="+encodeURIComponent(JSON.stringify(json));
             window.open(url,"_blank");  
+
         },
         showError: function(msg){
             this.$message({
