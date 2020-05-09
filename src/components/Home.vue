262bae82f5d96ae1ff16c8ce5e6535c20f20ef9c
liuchang1a2a <141303@supinfo.com>
2020-05-09 14:55:33 +0800

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,9 @@
         <h1>Home</h1>
         <p>{{msg}}</p>
         
-        <router-link to="/home/checkcode">CheckCode</router-link>
-        <router-link to="/home/markimg">MarkImg</router-link>
-        <router-link to="/home/qrcode">QrCode</router-link>
+        <router-link to="/home/checkcode" title="验证码">CheckCode</router-link>
+        <router-link to="/home/markimg" title="水印">MarkImg</router-link>
+        <router-link to="/home/qrcode"  title="二维码">QrCode</router-link>
         <router-view></router-view>
     </div>
 </template>
